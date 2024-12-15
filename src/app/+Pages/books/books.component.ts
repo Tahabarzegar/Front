6b30c5b62cf9378b1a873ebace7677e8a16faad9
FFounder <mohammadtahabarzegar188@gmail.com>
2024-12-15 23:37:58 +0330

    finallization

---
+++ src/app/+Pages/books/books.component.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-books',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css'
+})
+export class BooksComponent implements OnInit {
+  http = inject(HttpClient);
+  data: any[] = [];
+  action = 'list';
+  entity = {
+    title: '',
+    writer: '',
+    price: 0
+  };
+  ngOnInit(): void {
+    this.refresh();
+  }
+  refresh(){
+    this.http.get('http://localhost:5042/books/list').subscribe(res => {
+      this.data = res as any[];
+    });
+  }
+  addClick() {
+    this.action = 'add';
+  }
+  cancel() {
+    this.action = 'list';
+  }
+  ok() {
+    if (this.action == 'add'){
+      this.http.post('http://localhost:5042/books/add',this.entity).subscribe(res=>{
+        this.action='list';
+        this.refresh();
+      });
+    }
+  }
+  editClicK(item:any) {
+    this.action='edit';
+    this.entity=item;
+    if (this.action == 'edit'){
+      this.http.post('http://localhost:5042/books/add',this.entity).subscribe(res=>{
+        this.action='list';
+        this.refresh();
+      });
+    }
+  }
+  
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { BooksComponent } from './+Pages/books/books.component';
+import { BorrowsComponent } from './+Pages/borrows/borrows.component';
+import { MembersComponent } from './+Pages/members/members.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'books', component: BooksComponent },
+    { path: 'members', component: MembersComponent },
+    { path: 'borrows', component: BorrowsComponent },
+    { path: '', redirectTo: '/books', pathMatch: 'full' },
+    { path: '**', redirectTo: '/books' }
+];
