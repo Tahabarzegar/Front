9d9ab7c43ceff0d59f37f35ea392fc6646a8ef6a
FFounder <mohammadtahabarzegar188@gmail.com>
2024-12-16 01:22:47 +0330

    finallization

--- src/app/+Pages/books/books.component.ts
+++ src/app/+Pages/books/books.component.ts
@@ -21,7 +21,7 @@ export class BooksComponent implements OnInit {
   ngOnInit(): void {
     this.refresh();
   }
-  refresh(){
+  refresh() {
     this.http.get('http://localhost:5042/books/list').subscribe(res => {
       this.data = res as any[];
     });
@@ -33,22 +33,38 @@ export class BooksComponent implements OnInit {
     this.action = 'list';
   }
   ok() {
-    if (this.action == 'add'){
-      this.http.post('http://localhost:5042/books/add',this.entity).subscribe(res=>{
-        this.action='list';
+    if (this.action == 'add') {
+      this.http.post('http://localhost:5042/books/add', this.entity).subscribe(res => {
+        this.action = 'list';
+        this.refresh();
+      });
+    }
+    else if (this.action == 'edit') {
+      this.http.post('http://localhost:5042/books/update', this.entity).subscribe(res => {
+        this.action = 'list';
+        this.refresh();
+        });
+  }
+  }
+  editClicK(item: any) {
+    this.action = 'edit';
+    this.entity = item;
+    if (this.action == 'edit') {
+      this.http.post('http://localhost:5042/books/list', this.entity).subscribe(res => {
+        this.action = 'list';
         this.refresh();
       });
     }
   }
-  editClicK(item:any) {
-    this.action='edit';
-    this.entity=item;
-    if (this.action == 'edit'){
-      this.http.post('http://localhost:5042/books/add',this.entity).subscribe(res=>{
-        this.action='list';
+
+  Delete(item: any) {
+    this.action = 'delete';
+    this.entity = item;
+    if (this.action == 'delete') {
+      this.http.post('http://localhost:5042/books/remove', this.entity).subscribe(res => {
+        this.action = 'list';
         this.refresh();
       });
     }
   }
-  
 }
